14e972250bd16930bbee8c902e150087eb5f9125
hassanlmezher <hassanmezher084@gmail.com>
2025-10-16 23:00:16 +0300

    Protected Routes

--- backend/server.js
+++ backend/server.js
@@ -38,3 +38,44 @@ const authenticateToken = (req, res, next) => {
     return res.status(403).json({ message: 'Invalid or expired token' });
   }
 };
+
+app.post ('/api/register', async (req, res) => {
+  const { name, email, password } = res.body;
+  try{
+    const userExists = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    if(userExists.rows.length > 0) return res.status(400).json({ message: 'User already exists' });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id, name, email', [name, email, hashedPassword]);
+
+    const token = generateToken(newUser.rows[0].id);
+    res.status(201).json({ message: 'User registered successfully', user: newUser.rows[0].id, token, });
+  } catch (err) {
+    console.error('Register Error:'. err);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
+app.post('api/login', async (req, res) => {
+  const { email, password } = req.body;
+  try{
+    const user = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    if(user.rows.length === 0) return res.status(400).json({ message: 'Invalid email or password' });
+
+    const validPass = await bcrypt.compare(password, user.rows[0].password);
+    if(!validPass) return res.status(400).json({ message: "Invalid email or password" });
+    const token = generateToken(user.rows[0].id);
+    res.json({
+      message: 'Login successful',
+      user: {
+        id: user.rows[0].id,
+        name: user.rows[0].name,
+        email: user.rows[0].email,
+      },
+      token,
+    });
+  } catch(err) {
+    console.error('Login Error:', err);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
